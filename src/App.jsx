dede8b9efe6ef3026325dde38620a02fcc00bfcc
IlhamKurniawan143 <ilhamkur143@gmail.com>
2023-11-20 12:56:00 +0700

    menambahkan halaman pembayaran gagal dan update halaman pembayaran sukses

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet"
 import NavbarComponent from "./components/NavbarComponent";
 import FooterComponent from "./components/FooterComponent"
-
 import Login from "./pages/Login";
+import Daftar from "./pages/Daftar";
 import HomePage from "./pages/HomePage";
 import KelasPage from "./pages/KelasPage";
 import TestimoniPage from "./pages/TestimoniPage";
@@ -14,15 +14,23 @@ import SyaratKetenPage from "./pages/SyaratKetenPage";
 function App() {
 
   return <div>
-    
-    <Routes>
-      <Route path="/login" Component={Login}/>
-      <Route path="/" Component={HomePage}/>
-      <Route path="/kelas" Component={KelasPage}/>
-      <Route path="/testimoni" Component={TestimoniPage}/>
-      <Route path="/faq" Component={FaqPage}/>
-      <Route path="/syaratketen" Component={SyaratKetenPage}/>
-    </Routes>
+    <BrowserRouter>
+      <Helmet>
+				<title>Bijakcuan.</title>
+			</Helmet>
+
+      <NavbarComponent />
+        <Routes>
+          <Route path="/login" Component={Login}/>
+          <Route path="/Daftar" Component={Daftar}/>
+          <Route path="/" Component={HomePage}/>
+          <Route path="/kelas" Component={KelasPage}/>
+          <Route path="/testimoni" Component={TestimoniPage}/>
+         <Route path="/faq" Component={FaqPage}/>
+         <Route path="/syaratketen" Component={SyaratKetenPage}/>
+        </Routes>
+       <FooterComponent />
+    </BrowserRouter>
   </div>
   
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,71 @@
-import { Form } from "react-bootstrap";
+import "./css/daftar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router-dom"
 
-import React from 'react'
 
-const Login = () => {
+export default function Login() {
   return (
-        <Form>
-        <h1 className='mb-4' >NusaTech</h1>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      </Form>
-
-  )
+		<main id="masuk">
+			<div className="container py-5 h-100">
+				<div className="row d-flex justify-content-center align-items-center h-100">
+					<div className="col-12 col-md-8 col-lg-6 col-xl-6">
+						<div
+							className="card shadow-3 px-5"
+							style={{ borderRadius: "1rem" }}>
+							<div className="card-body p-5 text-center">
+              <FontAwesomeIcon icon={faEnvelope} />
+								<img
+									src="React.svg"
+									alt="Logo BijakCuan"
+									style={{
+										borderRadius: "2rem",
+										height: "100px",
+										width: "100px",
+									}}
+									className="mb-4"
+								/>
+								<h3 className="mb-1 fw-bold">Masuk</h3>
+								<p className="mb-4 ">Kembali bertumbuh bersama BijakCuan</p>
+								
+                <div className="d-flex flex-column gap-4">
+									<form
+										className="form-outline d-flex flex-column gap-2">
+										<input
+											type="text"
+											name="username"
+											id="username"
+											placeholder="Username/email"
+                      className="mt-5"
+											required
+										/>
+										<input
+											type="password"
+											name="password"
+											id="password"
+											placeholder="Password"
+                      className="border radius = 10 px;"
+											required
+										/>
+										<button type="submit" className="btn btn-primary mt-3">
+											Masuk
+										</button>
+									</form>
+								
+									<p>
+										Belum memiliki akun?{" "}
+										<Link
+											to="/daftar"
+											className="text-dark text-decoration-underline">
+											Daftar sekarang
+										</Link>
+									</p>
+								</div>
+					</div>
+				</div>
+        </div>
+							</div>
+						</div>
+		</main>
+	)
 }
-
-export default Login
